b526a641f8adb9f4f9c1be7a8886bf9f9cf377d6
Falcon <falcon@adawarp.com>
2023-01-26 18:04:31 +0900

    昨日からの追加機能 編集ボタン押下でIPアドレスが編集できる状態になる 保存ボタン押下で指定したIPアドレスのカメラ映像が確認できる 保存したIPはlocalstrageで保存できる CSSで見た目の変更

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const savebtn = document.getElementById('savebtn');
-const inputtag = document.getElementById('input-ip-a');
+const inputtag = document.createElement('input');
+inputtag.setAttribute('class','input-ip-a');
+var data = localStorage.getItem('data');
+inputtag.setAttribute('value',data);
 const cameraview = document.getElementById('iframe-1');
+const ptag = document.createElement('p');
+ptag.setAttribute('class','p-ip-a');
+const div = document.querySelector('div');
 var ipaddress;
+
+div.appendChild(ptag);
+ptag.textContent = data;
 
 inputtag.addEventListener('input', (event) => {
   
@@ -11,11 +20,21 @@ inputtag.addEventListener('input', (event) => {
 savebtn.addEventListener('click', () => {
   const iframe_src = `http://${ipaddress}:7011/ImageViewer?Mode=Motion&Resolution=320x240&Quality=Standard&Interval=10`
   cameraview.src = iframe_src
-  console.log(cameraview.src)
   // savebtn.textContent="NO";
 
   if(savebtn.textContent=="編集"){
-    savebtn.textContent="保存";}
+    savebtn.textContent="保存";
+    ptag.remove();
+    ptag.textContent = data;
+    div.appendChild(inputtag);
+  }
   else if(savebtn.textContent=="保存"){
-    savebtn.textContent="編集";}
+    savebtn.textContent="編集";
+    inputtag.remove();
+    localStorage.setItem('data', inputtag.value);
+    var data = localStorage.getItem('data');
+    ptag.textContent = data;
+    div.appendChild(ptag);
+  
+  }
 })
